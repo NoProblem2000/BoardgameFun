5e89e2dabee3f944a83d247a34ac3b6aaadd19a9
i.i.artamonov <fernando94@yandex.ru>
2021-11-11 14:57:38 +0400

    initial commit

---
+++ src/main/java/com/petproject/boardgamefun/BoardgamefunApplication.java
@@ -0,0 +1,13 @@
+package com.petproject.boardgamefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgamefunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardgamefunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petproject/boardgamefun/BoardgamefunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.boardgamefun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgamefunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

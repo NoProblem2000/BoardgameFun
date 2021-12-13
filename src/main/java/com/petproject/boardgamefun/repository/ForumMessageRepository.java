708aa6530e66e1f556bdb561044a5855055f0513
i.i.artamonov <fernando94@yandex.ru>
2021-12-13 09:28:44 +0400

    Added a missing file from the previous commit
    commt 9e3badd2a4f4952b9dea411ecede077b2aee080b

--- src/main/java/com/petproject/boardgamefun/repository/ForumMessageRepository.java
+++ src/main/java/com/petproject/boardgamefun/repository/ForumMessageRepository.java
@@ -3,5 +3,10 @@ package com.petproject.boardgamefun.repository;
 import com.petproject.boardgamefun.model.ForumMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ForumMessageRepository extends JpaRepository<ForumMessage, Integer> {
+    List<ForumMessage> findByUserId(Integer id);
+    List<ForumMessage> findByForumId(Integer id);
+    ForumMessage findForumMessageById(Integer id);
 }

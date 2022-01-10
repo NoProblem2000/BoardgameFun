3787d91dd426a4addaa173cfda22f0576aef86ee
i.i.artamonov <fernando94@yandex.ru>
2022-01-10 10:04:19 +0400

    Change the DesignerDTO
    Add Id to DTO. Adding designerService. Change returning type of methods in DesignerController

--- src/main/java/com/petproject/boardgamefun/controller/DesignerController.java
+++ src/main/java/com/petproject/boardgamefun/controller/DesignerController.java
@@ -1,8 +1,10 @@
 package com.petproject.boardgamefun.controller;
 
+import com.petproject.boardgamefun.dto.DesignerDTO;
 import com.petproject.boardgamefun.dto.request.DesignerRequest;
 import com.petproject.boardgamefun.model.Designer;
 import com.petproject.boardgamefun.repository.DesignerRepository;
+import com.petproject.boardgamefun.service.DesignerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,61 +17,63 @@ import java.util.List;
 public class DesignerController {
 
     private final DesignerRepository designerRepository;
+    private final DesignerService designerService;
 
-    public DesignerController(DesignerRepository designerRepository) {
+    public DesignerController(DesignerRepository designerRepository, DesignerService designerService) {
         this.designerRepository = designerRepository;
+        this.designerService = designerService;
     }
 
     @Transactional
     @GetMapping("")
-    public ResponseEntity<List<Designer>> getDesigners(){
-        var designers = designerRepository.findAll();
+    public ResponseEntity<List<DesignerDTO>> getDesigners(){
+        var designers =  designerService.entitiesToDesignerDTO(designerRepository.findAll());
 
         return new ResponseEntity<>(designers, HttpStatus.OK);
     }
 
     @Transactional
     @GetMapping("/{name}")
-    public ResponseEntity<Designer> getDesignerByName(@PathVariable String name){
-        var designers = designerRepository.findDesignerByName(name);
+    public ResponseEntity<DesignerDTO> getDesignerByName(@PathVariable String name){
+        var designer = designerService.entityToDesignerDTO(designerRepository.findDesignerByName(name));
 
-        return new ResponseEntity<>(designers, HttpStatus.OK);
+        return new ResponseEntity<>(designer, HttpStatus.OK);
     }
 
     @Transactional
     @PostMapping("/add")
-    public ResponseEntity<List<Designer>> addDesigner(@RequestBody DesignerRequest designerRequest){
+    public ResponseEntity<List<DesignerDTO>> addDesigner(@RequestBody DesignerRequest designerRequest){
         var designer = new Designer();
         designer.setName(designerRequest.getName());
         designerRepository.save(designer);
 
-        var designers = designerRepository.findAll();
+        var designers = designerService.entitiesToDesignerDTO(designerRepository.findAll());
 
         return new ResponseEntity<>(designers, HttpStatus.OK);
     }
 
     @Transactional
     @PatchMapping("/update/{id}")
-    public ResponseEntity<List<Designer>> updateDesigner(@PathVariable Integer id, @RequestBody DesignerRequest designerRequest){
+    public ResponseEntity<List<DesignerDTO>> updateDesigner(@PathVariable Integer id, @RequestBody DesignerRequest designerRequest){
         var designer = designerRepository.findDesignerById(id);
 
         designer.setName(designerRequest.getName());
         designerRepository.save(designer);
 
-        var designers = designerRepository.findAll();
+        var designers = designerService.entitiesToDesignerDTO(designerRepository.findAll());
 
         return new ResponseEntity<>(designers, HttpStatus.OK);
     }
 
     @Transactional
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<List<Designer>> deleteDesigner(@PathVariable Integer id){
+    public ResponseEntity<List<DesignerDTO>> deleteDesigner(@PathVariable Integer id){
 
         var designer = designerRepository.findDesignerById(id);
 
         designerRepository.delete(designer);
 
-        var designers = designerRepository.findAll();
+        var designers = designerService.entitiesToDesignerDTO(designerRepository.findAll());
 
         return new ResponseEntity<>(designers, HttpStatus.OK);
     }
--- src/main/java/com/petproject/boardgamefun/dto/DesignerDTO.java
+++ src/main/java/com/petproject/boardgamefun/dto/DesignerDTO.java
@@ -6,5 +6,6 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class DesignerDTO {
+    private Integer id;
     private String designer;
 }

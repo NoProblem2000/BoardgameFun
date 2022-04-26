9a0b6fc240d908fcb8ce1f02de0f0461a40f59c2
i.i.artamonov <fernando94@yandex.ru>
2022-04-26 08:46:58 +0400

    Added file to resolve final kotlin classes
    Now we can mock the final classes of kotlin

--- src/main/java/com/petproject/boardgamefun/service/DiaryService.kt
+++ src/main/java/com/petproject/boardgamefun/service/DiaryService.kt
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service
 import java.util.ArrayList
 
 @Service
-open class DiaryService {
-    open fun projectionsToDiaryDTO(projections: List<DiaryWithRatingsProjection>): List<DiaryDTO> {
+class DiaryService {
+    fun projectionsToDiaryDTO(projections: List<DiaryWithRatingsProjection>): List<DiaryDTO> {
         val diaries: MutableList<DiaryDTO> = ArrayList()
         for (projection in projections) {
             diaries.add(DiaryDTO(projection.diary, projection.rating))
@@ -16,11 +16,11 @@ open class DiaryService {
         return diaries
     }
 
-    open fun entityToDiaryDTO(diary: Diary): DiaryDTO {
+    fun entityToDiaryDTO(diary: Diary): DiaryDTO {
         return DiaryDTO(diary, 0.0)
     }
 
-    open fun projectionToDiaryDTO(projection: DiaryWithRatingsProjection): DiaryDTO {
+    fun projectionToDiaryDTO(projection: DiaryWithRatingsProjection): DiaryDTO {
         return DiaryDTO(projection.diary, projection.rating)
     }
 }

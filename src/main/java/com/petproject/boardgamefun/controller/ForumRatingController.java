9fc53a28e5072127becd2dff177386d13e06a366
i.i.artamonov <fernando94@yandex.ru>
2022-09-26 08:46:15 +0400

    Added swagger docs annotation to ForumRatingController

--- src/main/java/com/petproject/boardgamefun/controller/ForumRatingController.java
+++ src/main/java/com/petproject/boardgamefun/controller/ForumRatingController.java
@@ -3,6 +3,9 @@ package com.petproject.boardgamefun.controller;
 import com.petproject.boardgamefun.dto.ForumDataDTO;
 import com.petproject.boardgamefun.dto.request.ForumRatingRequest;
 import com.petproject.boardgamefun.service.ForumRatingService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,6 +21,13 @@ public class ForumRatingController {
         this.forumRatingService = forumRatingService;
     }
 
+    @ApiOperation
+            (value = "Set rating for forum by user", notes = "Return forum model with rating")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Rating successfully set"),
+            @ApiResponse(code = 404, message = "Forum with that id does not exist or User with that id does not exist"),
+            @ApiResponse(code = 401, message = "You are not authorized")
+    })
     @PostMapping("/{forumId}/{userId}")
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<ForumDataDTO> setForumRating(@PathVariable Integer forumId, @PathVariable Integer userId, @RequestBody ForumRatingRequest forumRatingRequest) {
@@ -25,6 +35,13 @@ public class ForumRatingController {
         return new ResponseEntity<>(forum, HttpStatus.OK);
     }
 
+    @ApiOperation
+            (value = "Remove rating for forum by user", notes = "Return forum model with rating")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Rating successfully removed"),
+            @ApiResponse(code = 404, message = "Forum with that id does not exist or User with that id does not exist"),
+            @ApiResponse(code = 401, message = "You are not authorized")
+    })
     @DeleteMapping("/{forumId}/{ratingId}")
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<ForumDataDTO> removeRatingFromForum(@PathVariable Integer forumId, @PathVariable Integer ratingId) {

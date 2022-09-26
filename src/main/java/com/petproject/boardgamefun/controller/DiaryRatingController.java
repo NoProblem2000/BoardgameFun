66e7740ecc4eb5d29e32e95c38d0f236d95a93be
i.i.artamonov <fernando94@yandex.ru>
2022-09-26 09:22:33 +0400

    Added swagger docs annotations to DiaryRatingController

--- src/main/java/com/petproject/boardgamefun/controller/DiaryRatingController.java
+++ src/main/java/com/petproject/boardgamefun/controller/DiaryRatingController.java
@@ -3,6 +3,9 @@ package com.petproject.boardgamefun.controller;
 import com.petproject.boardgamefun.dto.DiaryRatingDTO;
 import com.petproject.boardgamefun.dto.request.DiaryRatingRequest;
 import com.petproject.boardgamefun.service.DiaryRatingService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,6 +22,13 @@ public class DiaryRatingController {
         this.diaryRatingService = diaryRatingService;
     }
 
+    @ApiOperation
+            (value = "Set the diary rating", notes = "Return new rating")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Diary rating successfully set"),
+            @ApiResponse(code = 401, message = "You are not authorized"),
+            @ApiResponse(code = 404, message = "Diary or User not found")
+    })
     @PostMapping("/{diaryId}/{userId}")
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<DiaryRatingDTO> setDiaryRating(@PathVariable Integer diaryId, @PathVariable Integer userId, @RequestBody DiaryRatingRequest ratingRequest) {
@@ -26,6 +36,13 @@ public class DiaryRatingController {
         return new ResponseEntity<>(diaryRatingDTO, HttpStatus.OK);
     }
 
+    @ApiOperation
+            (value = "Update the diary rating", notes = "Return updated rating")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Diary rating successfully updated"),
+            @ApiResponse(code = 401, message = "You are not authorized"),
+            @ApiResponse(code = 404, message = "Diary rating not found")
+    })
     @PatchMapping("/{ratingId}")
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<DiaryRatingDTO> updateDiaryRating(@PathVariable Integer ratingId, @RequestBody DiaryRatingRequest ratingRequest) {
@@ -33,6 +50,13 @@ public class DiaryRatingController {
         return new ResponseEntity<>(diaryRatingDTO, HttpStatus.OK);
     }
 
+    @ApiOperation
+            (value = "Remove the diary rating", notes = "Return status of deleted rating")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Diary rating successfully removed"),
+            @ApiResponse(code = 401, message = "You are not authorized"),
+            @ApiResponse(code = 404, message = "Diary rating not found")
+    })
     @DeleteMapping("/{ratingId}")
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
     public ResponseEntity<String> deleteDiaryRating(@PathVariable Integer ratingId) {
